b06f80870c629bb0a5d0a0ae6f7d1246b2df7e70
lamborqq <lanbery@qq.com>
2019-03-26 18:28:22 +0800

    add gossip debug

--- nbs-grpc2s-lib/BBL/GossipServiceClient.cs
+++ nbs-grpc2s-lib/BBL/GossipServiceClient.cs
@@ -57,7 +57,7 @@ namespace grpc2slib.BBL
             string pcName = LocalInfoUtil.GetLocalUsername();
             builder.Append("\r\n");
             builder.Append("┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓\r\n");
-            builder.Append("┃************************* Gossip Debug Info *************************┃\r\n");
+            builder.Append("┃************************* Gossip Debug Info *************************\r\n");
             builder.AppendFormat("┃ Operator	:{0}	\r\n", pcName);
             builder.AppendFormat("┃ LocalIP	:{0}	\r\n", ipv4);
             builder.Append("┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛\r\n");
@@ -67,68 +67,19 @@ namespace grpc2slib.BBL
                 viewInfos infos = result.InputViews;
                 int len = result.InputViews.Views.Count;
                 builder.AppendFormat("#####>>>{0}:{1}>>>Begin:\r\n", "IN", result.InputViews.Views.Count);
-                string pattern = @"\|";
-                
-                
+
                 for (int i = 0; i < len; i++)
                 {
                     string s = result.InputViews.Views[i];
-                    string[] vs = s.Split('|');
-                    StringBuilder isb = new StringBuilder();
-                    if (vs.Length > 0)
-                    {
-                        for(int m=0;m<vs.Length;m++)
-                        {
-                            if (m > 1)
-                            {
-                                isb.Append("|");
-                                isb.Append(vs[m]);
-                                isb.Append("|");
-                            }
-                            else
-                            {
-                                isb.Append(vs[m]);
-                            }
-                            isb.Append("\r\n");
-                        }
-                        builder.Append(vs.ToString());
-                    }
-                    else
-                    {
-                        builder.Append(s);
-                    }
-
+                    builder.Append(s);
                     builder.AppendLine();
                 }
                 viewInfos outviews = result.OutputViews;
                 builder.AppendFormat("#####>>>{0}:{1}>>>Begin:\r\n", "OUT", outviews.Views.Count);
                 for (int j = 0; j < outviews.Views.Count; j++)
                 {
-                    string s = outviews.Views[j];
-                    string[] vs = s.Split('|');
-                    StringBuilder isb = new StringBuilder();
-                    if (vs.Length > 0)
-                    {
-                        for (int m = 0; m < vs.Length; m++)
-                        {
-                            if (m > 1)
-                            {
-                                isb.Append("|");
-                                isb.Append(vs[m]);
-                                isb.Append("|");
-                            }
-                            else
-                            {
-                                isb.Append(vs[m]);
-                            }
-                            isb.Append("\r\n");
-                        }
-                        builder.Append(vs.ToString());
-                    }
-                    else
-                    {
-                        builder.Append(s);
-                    }
+                    string s = outviews.Views[j];                    
+                    builder.Append(s);
                     builder.AppendLine();
                 }
                 builder.AppendFormat("############ {0} ###########\r\n", now.ToString("yyyy-MM-dd HH:mm:ss"));
